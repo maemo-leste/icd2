8dd2b115b4545204f4b709d54f2aff3cd5e00ed0
Nicolas Aragone <zungo@icloud.com>
2018-02-21 21:40:17 -0300

    Commented icd_context.h as per maemo.org API reference

--- icd/icd_context.h
+++ icd/icd_context.h
@@ -4,30 +4,30 @@
 #include <glib.h>
 
 struct icd_context {
-  gboolean daemon;
-  guint shutting_down;
-  GMainLoop *main_loop;
+  gboolean daemon; //run as daemon if TRUE, run in foreground if FALSE
+  guint shutting_down; //icd shutdown timeout id
+  GMainLoop *main_loop; //glib main loop
 
-  GSList *policy_module_list;
+  GSList *policy_module_list; //list of policy modules
 
-  GSList *request_list;
+  GSList *request_list; //list of outstanding network connection requests
 
-  GSList *nw_module_list;
-  GHashTable *type_to_module;
+  GSList *nw_module_list; //list of network modules
+  GHashTable *type_to_module; //hash table mapping network types to the modules in the above list
 
-  GSList *srv_module_list;
-  GHashTable *srv_type_to_srv_module;
-  GHashTable *nw_type_to_srv_module;
+  GSList *srv_module_list; //list of service provider modules
+  GHashTable *srv_type_to_srv_module; //hash table mapping service provider types to service modules
+  GHashTable *nw_type_to_srv_module; //hash table mapping network types to list of service modules
 
-  guint idle_timer_notify;
+  guint idle_timer_notify; //idle time gconf notification id
 
-  guint iap_deletion_notify;
+  guint iap_deletion_notify; //IAP deletion gconf notification id
 };
 
-gboolean icd_context_init (void);
-struct icd_context *icd_context_get(void);
-void icd_context_run(void);
-void icd_context_stop(void);
-void icd_context_destroy (void);
+gboolean icd_context_init (void); //Initialize context
+struct icd_context *icd_context_get(void); //return the global context
+void icd_context_run(void); //start the main loop
+void icd_context_stop(void); //stop running the main loop
+void icd_context_destroy (void); //destroy context
 
 #endif
